d1cfc6d7337dd52e22e35527870b793dce6eec76
Safia Abdalla <safia@safia.rocks>
2021-04-06 11:40:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# preview2
+
+Deserialize 0: 35.755 milliseconds
+Deserialize 1: 1 milliseconds
+Deserialize 2: 0.8749 milliseconds
+Deserialize 3: 0.9599 milliseconds
+Deserialize 4: 0.83 milliseconds
+Deserialize 5: 0.92 milliseconds
+Deserialize 6: 0.88 milliseconds
+Deserialize 7: 0.79 milliseconds
+Deserialize 8: 0.835 milliseconds
+Deserialize 9: 0.915 milliseconds
+Deserialize 10: 0.79 milliseconds
+Serialize 0: 11.455 milliseconds
+Serialize 1: 1.71 milliseconds
+Serialize 2: 1.545 milliseconds
+Serialize 3: 1.545 milliseconds
+Serialize 4: 1.7499 milliseconds
+Serialize 5: 1.795 milliseconds
+Serialize 6: 1.875 milliseconds
+Serialize 7: 1.755 milliseconds
+Serialize 8: 1.585 milliseconds
+Serialize 9: 1.625 milliseconds
+Serialize 10: 1.545 milliseconds
+
+# preview3
+
+Deserialize 0: 43.25 milliseconds
+Deserialize 1: 3.25 milliseconds
+Deserialize 2: 3.375 milliseconds
+Deserialize 3: 3.04 milliseconds
+Deserialize 4: 3 milliseconds
+Deserialize 5: 3 milliseconds
+Deserialize 6: 3.165 milliseconds
+Deserialize 7: 3.04 milliseconds
+Deserialize 8: 3 milliseconds
+Deserialize 9: 3 milliseconds
+Deserialize 10: 3.085 milliseconds
+Serialize 0: 11.8299 milliseconds
+Serialize 1: 2.085 milliseconds
+Serialize 2: 1.875 milliseconds
+Serialize 3: 1.875 milliseconds
+Serialize 4: 2.085 milliseconds
+Serialize 5: 1.96 milliseconds
+Serialize 6: 2.04 milliseconds
+Serialize 7: 1.9199 milliseconds
+Serialize 8: 2.125 milliseconds
+Serialize 9: 1.8749 milliseconds
+Serialize 10: 1.8299 milliseconds
---
+++ preview2/TestApp/Program.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Diagnostics;
+
+namespace TestApp
+{
+    public class Program
+    {
+        public static string Data = @"[
+            {
+                ""Date"": ""2018-05-06"",
+                ""TemperatureC"": 1,
+                ""Summary"": ""Freezing""
+            },
+            {
+                ""Date"": ""2018-05-07"",
+                ""TemperatureC"": 14,
+                ""Summary"": ""Bracing""
+            },
+            {
+                ""Date"": ""2018-05-08"",
+                ""TemperatureC"": -13,
+                ""Summary"": ""Freezing""
+            },
+            {
+                ""Date"": ""2018-05-09"",
+                ""TemperatureC"": -16,
+                ""Summary"": ""Balmy""
+            },
+            {
+                ""Date"": ""2018-05-10"",
+                ""TemperatureC"": -2,
+                ""Summary"": ""Chilly""
+            },
+                {
+                ""Date"": ""2018-05-06"",
+                ""TemperatureC"": 1,
+                ""Summary"": ""Freezing""
+            },
+            {
+                ""Date"": ""2018-05-07"",
+                ""TemperatureC"": 14,
+                ""Summary"": ""Bracing""
+            },
+            {
+                ""Date"": ""2018-05-08"",
+                ""TemperatureC"": -13,
+                ""Summary"": ""Freezing""
+            },
+            {
+                ""Date"": ""2018-05-09"",
+                ""TemperatureC"": -16,
+                ""Summary"": ""Balmy""
+            },
+            {
+                ""Date"": ""2018-05-10"",
+                ""TemperatureC"": -2,
+                ""Summary"": ""Chilly""
+            }
+        ]";
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            WeatherForecast[] weatherForecast = null;
+            
+            for (var i = 0; i <= 10; i++)
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                weatherForecast = JsonSerializer.Deserialize<WeatherForecast[]>(Data);
+                stopWatch.Stop();
+
+                System.Console.WriteLine($"Deserialize {i}: {stopWatch.Elapsed.TotalMilliseconds} milliseconds");
+            }
+
+
+            for (var i = 0; i <= 10; i++)
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                byte[] jsonUtf8Bytes;
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                };
+                jsonUtf8Bytes = JsonSerializer.SerializeToUtf8Bytes(weatherForecast, options);
+                stopWatch.Stop();
+
+                System.Console.WriteLine($"Serialize {i}: {stopWatch.Elapsed.TotalMilliseconds} milliseconds");
+            }
+            
+            await builder.Build().RunAsync();
+        }
+    }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string Summary { get; set; }
+    }
+}
